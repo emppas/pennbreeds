3113224224453e3d69afb2272f45e29732d674eb
Paul Okonko <contactokonko@gmail.com>
2024-08-18 20:17:13 +0100

    added title to form

--- users/forms.py
+++ users/forms.py
@@ -4,10 +4,12 @@ from django.contrib.auth.forms import UserCreationForm
 
 
 class UserRegisterForm(UserCreationForm):
+    title = forms.Field()
     email = forms.EmailField()
     first_name = forms.Field()
     last_name = forms.Field()
     
+    
     class Meta:
         model = User
-        fields = ['username', 'email', 'first_name', 'last_name', 'password1', 'password2']
+        fields = ['username', 'title', 'first_name', 'last_name', 'email', 'password1', 'password2']
