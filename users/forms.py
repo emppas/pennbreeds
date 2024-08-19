43c69db226514ebf3065cf62d1c2a025e777e2aa
Paul Okonko <contactokonko@gmail.com>
2024-08-19 02:27:05 +0100

    adding more features to the back end

--- users/forms.py
+++ users/forms.py
@@ -1,10 +1,10 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import Profile
 
 
 class UserRegisterForm(UserCreationForm):
-    title = forms.Field()
     email = forms.EmailField()
     first_name = forms.Field()
     last_name = forms.Field()
@@ -12,4 +12,9 @@ class UserRegisterForm(UserCreationForm):
     
     class Meta:
         model = User
-        fields = ['username', 'title', 'first_name', 'last_name', 'email', 'password1', 'password2']
+        fields = ['username', 'first_name', 'last_name', 'email', 'password1', 'password2']
+        
+    class ProfileForm(forms.ModelForm):
+        class Meta:
+            model = Profile
+            fields = ['title', 'image']
--- users/models.py
+++ users/models.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100, null=True, blank=True)
     image = models.ImageField(default='default.jpg', upload_to='profile_pics')
     
     def __str__(self):
