ceb8d237ec9e225eb746ee2561857d22cf694a64
emppas <contactokonko@gmail.com>
2024-09-07 04:00:45 +0100

    gallery completed

--- gallery/urls.py
+++ gallery/urls.py
@@ -5,6 +5,5 @@ from . import views
 
 urlpatterns = [
     path('categories/', views.category_list, name='category_list'),
-    path('events/<str:category_name>/', views.event_list, name='event_list'),
-    path('event/<int:event_id>/', views.event_detail, name='event_detail'),
+  
 ]
--- gallery/views.py
+++ gallery/views.py
@@ -1,40 +1,21 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render
 from .models import Event, Image
-
-# Create your views here.
-
 
 def category_list(request):
     # Get all distinct categories from Event model
     categories = Event.objects.values_list('category', flat=True).distinct()
     
-     # Get all images for the "All" tab
-    # all_images = Image.objects.all()
-    
     # Create a dictionary to hold images by category
     category_images = {}
     for category in categories:
-        category_images[category] = Image.objects.filter(event__category=category)
+        images = Image.objects.filter(event__category=category)
+        category_images[category] = images
     
-       
+    # Set the default category
+    default_category = categories[0] if categories else None
+    
     return render(request, 'gallery/category_list.html', {
         'categories': categories,
         'category_images': category_images,
-        # 'all_images': all_images,  # Pass all images to the "All" tab
+        'default_category': default_category
     })
-
-
-
-
-def event_list(request, category_name):
-    events = Event.objects.filter(category=category_name)
-    return render(request, 'gallery/event_list.html', {'category': category_name, 'events': events})
-
-
-
-
-
-def event_detail(request, event_id):
-    event = get_object_or_404(Event, id=event_id)
-    images = event.images.all()
-    return render(request, 'gallery/event_detail.html', {'event': event, 'images': images})
